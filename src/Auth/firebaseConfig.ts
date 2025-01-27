a485a09b49200cc0270d4a3441f0be44ce27625f
Daniel Karanja <sirkaranja12@gmail.com>
2025-01-27 18:15:30 +0300

    firebase auth and login

---
+++ src/Auth/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5VxV08pI50lfPzv8H0v9PXJ_hUo0NgPg",
+  authDomain: "movies-4b0d1.firebaseapp.com",
+  projectId: "movies-4b0d1",
+  storageBucket: "movies-4b0d1.firebasestorage.app",
+  messagingSenderId: "789855615466",
+  appId: "1:789855615466:web:e43d8eb84e88f262391a93"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
